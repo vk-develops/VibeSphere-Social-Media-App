54e5e3ffb6dee44829394d694555c07e63a9a0c8
vk-develops <vimalkumarv291@gmail.com>
2024-08-03 17:43:06 +0530

    added the onboarding screen

--- Frontend/src/Screens/Onboarding/OnboardingScreen.js
+++ Frontend/src/Screens/Onboarding/OnboardingScreen.js
@@ -1,10 +1,50 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { FlatList, View, Dimensions } from "react-native";
+import React, { useRef, useState } from "react";
+import OnboardComponent from "../../Components/OnboardComponent";
+import { slideData } from "../../Data/SlideData";
+import { StatusBar } from "expo-status-bar";
 
-const OnboardingScreen = () => {
+const OnboardingScreen = ({ navigation }) => {
+    const slideRef = useRef();
+
+    const width = Dimensions.get("window").width;
+
+    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+    const updateCurrentSlideIndex = (e) => {
+        const contentOffsetX = e.nativeEvent.contentOffset.x;
+        const currentIndex = Math.round(contentOffsetX / width);
+        setCurrentSlideIndex(currentIndex);
+    };
+
+    const goToNextSlide = () => {
+        const nextSlideIndex = currentSlideIndex + 1;
+        if (nextSlideIndex != slideData.length) {
+            const offset = nextSlideIndex * width;
+            slideRef?.current.scrollToOffset({ offset });
+            setCurrentSlideIndex(currentSlideIndex + 1);
+        }
+    };
+
     return (
-        <View>
-            <Text>OnboardingScreen</Text>
+        <View className="flex-1">
+            <StatusBar backgroundColor="#f3f1ff" />
+            <FlatList
+                ref={slideRef}
+                onMomentumScrollEnd={updateCurrentSlideIndex}
+                showsHorizontalScrollIndicator={false}
+                horizontal
+                data={slideData}
+                pagingEnabled
+                renderItem={({ item }) => (
+                    <OnboardComponent
+                        slideItem={item}
+                        goToNextSlide={goToNextSlide}
+                        currentSlideIndex={currentSlideIndex}
+                        navigation={navigation}
+                    />
+                )}
+            />
         </View>
     );
 };
