d4010b7c49669867cae3d5226135a92b3846384c
vk-develops <vimalkumarv291@gmail.com>
2024-08-03 17:34:31 +0530

    added the onboarding data

---
+++ Frontend/src/Data/SlideData.js
@@ -0,0 +1,26 @@
+export const slideData = [
+    {
+        id: "1",
+        image: require("../../assets/images/OnboardingImg-1.png"),
+        title: "Explore new travel destinations.",
+        description:
+            "Discover new travel destinations with Bliss Wayfarer's expert travel recommendations throughout your journey.",
+        button: "Next",
+    },
+    {
+        id: "2",
+        image: require("../../assets/images/OnboardingImg-2.png"),
+        title: "Your companion for every adventure.",
+        description:
+            "Whether exploring a city or hiking mountains, Bliss Wayfarer offers the guidance you need for a successful trip.",
+        button: "Next",
+    },
+    {
+        id: "3",
+        image: require("../../assets/images/OnboardingImg-3.png"),
+        title: "Your dream trip with a Best itinerary.",
+        description:
+            "Create a personalized itinerary with all your must-see destinations and activities to craft unforgettable travel experiences",
+        button: "Continue",
+    },
+];
