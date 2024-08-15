737c044b1d1babf9f4d98b84bb6cf6984dc54584
vk-develops <vimalkumarv291@gmail.com>
2024-08-15 12:29:28 +0530

    added the handle google button component

--- Frontend/src/Components/GoogleLoginButton.js
+++ Frontend/src/Components/GoogleLoginButton.js
@@ -1,14 +1,62 @@
 import { Button } from "react-native";
-import React from "react";
-import * as AuthSession from "expo-auth-session";
+import React, { useEffect } from "react";
+import { useAuthRequest, makeRedirectUri } from "expo-auth-session";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "../Redux/Features/usersAuthSlice";
 
 const GOOGLE_CLIENT_ID = process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID;
 const AUTH_URL = "http://localhost:8080";
 
+const discovery = {
+    authorizationEndpoint: "https://accounts.google.com/o/oauth2/v2/auth",
+    tokenEndpoint: "https://oauth2.googleapis.com/token",
+    revocationEndpoint: "https://oauth2.googleapis.com/revoke",
+};
+
 const GoogleLoginButton = () => {
     const dispatch = useDispatch();
+
+    const [request, response, promptAsync] = useAuthRequest(
+        {
+            clientId: GOOGLE_CLIENT_ID,
+            scopes: ["profile", "email"],
+            redirectUri: makeRedirectUri({
+                native: "VibeSphere://redirect",
+            }),
+        },
+        discovery
+    );
+
+    useEffect(() => {
+        const handleAuthRequest = async () => {
+            if (response?.type === "success") {
+                console.log("Response: ", response);
+
+                const { id_token } = response.params;
+
+                try {
+                    const res = await fetch(
+                        "http://localhost:8080/api/v1/users/auth/google/callback",
+                        {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({ id_token }),
+                        }
+                    );
+
+                    const data = await res.json();
+
+                    console.log(data);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        };
+
+        handleAuthRequest();
+    }, [response]);
 
     return <Button title="Login with Google" />;
 };
